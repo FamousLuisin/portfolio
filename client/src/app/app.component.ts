e79efc4769c7b97b4c176c08bb55c8114656eef6
Luís Filipe <lufimelo@outlook.com>
2025-04-02 15:15:38 -0300

    feat: cria tela inicial

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, ProfileComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'client';
+}
